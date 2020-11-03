89eaf8d3717bea94331a7b26ca96df931ad3d686
codwerk <louis_widdy@live.fr>
2020-11-03 23:24:27 +0100

    4 components(Pokemon, PokemonDetail, PokemonList, PokemonSearch)

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,37 @@
+<template>
+ <div>
+   <h1 class="title has-text-centered">Pokemon's</h1>
+
+   <PokemonSearch></PokemonSearch>
+   <PokemonList :imageUrl="imageUrl" :apiUrl="apiUrl"></PokemonList>
+   <PokemonDetail></PokemonDetail>
+ </div>
+</template>
+
+<script>
+import PokemonSearch from './PokemonSearch'
+import PokemonList from './PokemonList'
+import PokemonDetail from './PokemonDetail'
+
+ export default {
+   data() {
+     return {
+       imageUrl: 'https://pokeres.bastionbot.org/images/pokemon/1.png',
+        apiUrl: 'https://pokeapi.co/api/v2/pokemon/'
+     }
+   },
+   components: {
+     PokemonSearch,
+     PokemonList,
+     PokemonDetail
+   },
+   methods: {
+
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/components/PokemonList.vue
@@ -0,0 +1,16 @@
+<template>
+ <div></div>
+</template>
+
+<script>
+ export default {
+   data(){
+     return {}
+   },
+   props: ['imageUrl', 'apiUrl']
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
